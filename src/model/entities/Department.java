20554faf5f62c31cd29431aab0b5afa9c3ee2db1
Mateus Eneas <mateus.eneas98@gmail.com>
2024-12-19 11:56:32 -0300

    Classe Department

---
+++ src/model/entities/Department.java
@@ -0,0 +1,58 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Department implements Serializable { //São transformados em sequencia de bytes, para serem trafegados em rede, gravado em arquivos e assim por diante
+
+    private Integer ID;
+    private String name;
+
+    public Department() {
+    }
+
+    public Department(Integer ID, String name) {
+        this.ID = ID;
+        this.name = name;
+    }
+
+    public Integer getID() {
+        return ID;
+    }
+
+    public void setID(Integer ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return Objects.equals(ID, that.ID);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ID);
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "ID=" + ID +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+
+
+}
