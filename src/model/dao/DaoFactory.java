ca8f100a0ad77bfc57e25dee9eaae304e598d188
Mateus Eneas <mateus.eneas98@gmail.com>
2024-12-28 17:30:52 -0300

    Implementando a classe DepartmentDaoJDBC e testando!

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,16 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
     public static SellerDao creaseSellerDao () {
         return new SellerDaoJDBC(DB.getConnection());
+    }
+
+    public static DepartmentDao creaseDepartmentDao() {
+        return new DepartmentDaoJDBC(DB.getConnection());
     }
 }
