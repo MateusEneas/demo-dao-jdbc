36028b9a7b0c451a7a7c67065ceaf3d8349f0394
Mateus Eneas <mateus.eneas98@gmail.com>
2024-12-28 17:00:51 -0300

    Implementando o delete

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,10 +7,13 @@ import model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
 
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         SellerDao sellerDao = DaoFactory.creaseSellerDao();
 
@@ -31,10 +34,10 @@ public class Program {
             System.out.println(obj);
         }
 
-        System.out.println("\n=== TEST 4: seller insert ===");
+        /*System.out.println("\n=== TEST 4: seller insert ===");
         Seller newSeller = new Seller(null, "Mateus", "mateus@gmail.com", new Date(), 4500.0, department);
         sellerDao.insert(newSeller);
-        System.out.println("Inserido! Novo id = " + newSeller.getId());
+        System.out.println("Inserido! Novo id = " + newSeller.getId()); */
 
         System.out.println("\n=== TEST 5: seller update ===");
         seller = sellerDao.findById(1);
@@ -44,5 +47,13 @@ public class Program {
         sellerDao.update(seller);
         System.out.println("Atualização completa!");
 
+        System.out.println("\n=== TEST 6: seller delete ===");
+        System.out.println("Entre com um ID para o teste: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.print("Funcionário deletado!");
+
+
+        sc.close();
     }
 }
